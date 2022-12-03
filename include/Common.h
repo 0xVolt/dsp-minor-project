2a988d891a68e7690b3168b0ec6e07cda410d559
Desh Iyer <desh.iyer@gmail.com>
2022-12-04 01:37:30 +0530

    Updated header file

--- include/Common.h
+++ include/Common.h
@@ -1,36 +1,34 @@
-// #ifndef DSPFILTERS_COMMON_H
-// #define DSPFILTERS_COMMON_H
+#ifndef DSPFILTERS_COMMON_H
+#define DSPFILTERS_COMMON_H
 
-// //
-// // This must be the first file included in every DspFilters header and source
-// //
+//
+// This must be the first file included in every DspFilters header and source
+//
 
-// #ifdef _MSC_VER
-// #  pragma warning (disable: 4100)
-// #endif
+#ifdef _MSC_VER
+#  pragma warning (disable: 4100)
+#endif
 
-// //#include <assert.h>
-// #include <stdlib.h>
-// #include <iomanip>
-// #include <sstream>
+//#include <assert.h>
+#include <stdlib.h>
+#include <iomanip>
+#include <sstream>
 
-// #include <cassert>
-// #include <cfloat>
-// #include <cmath>
-// #include <complex>
-// #include <cstring>
-// #include <string>
-// #include <limits>
-// #include <vector>
-// #include <algorithm>
+#include <cassert>
+#include <cfloat>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <string>
+#include <limits>
+#include <vector>
+#include <algorithm>
 
-void print();
-
-// #ifdef _MSC_VER
-// namespace tr1 = std::tr1;
-// #else
-// namespace tr1 = std;
-// #endif
+#ifdef _MSC_VER
+namespace tr1 = std::tr1;
+#else
+namespace tr1 = std;
+#endif
 
 
-// #endif
+#endif
