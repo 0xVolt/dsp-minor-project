5c41802414702a554d5a5f8e63beb2d1590b0730
Desh Iyer <desh.iyer@gmail.com>
2022-12-04 00:29:48 +0530

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
 #include "include/Common.h"
+
+#include <iostream>
+
+#define NUMBER_SAMPLES 2000
+#define NUMBER_CHANNELS 2
 
 using namespace std;
 
 int main() {
-    cout << "Hello, world!\n";
+    float *audioChannels[NUMBER_CHANNELS];
+
+    for (int i = 0; i < NUMBER_CHANNELS; ++i) {
+        audioChannels[i] = new float[NUMBER_SAMPLES];
+    }
 
     return 0;
 }
